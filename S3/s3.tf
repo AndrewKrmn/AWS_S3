200c9285d9825435c1ce218fbfaa6e2484f76ccd
Andrew <134720993+AndrewKrmn@users.noreply.github.com>
2023-07-21 17:44:11 +0300

    Create s3.tf

---
+++ S3/s3.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+    
+    region = "eu-north-1"
+}
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "kakashka" 
+
+  tags = {
+    Name        = "My Terraform Bucket"
+    Environment = "Production"
+  }
+}
+resource "aws_s3_bucket_object" "my_folder" {
+  bucket = "kakashka"
+  key    = "files/"
+  source = "files/lapka.txt"
+}
+resource "aws_s3_bucket_object" "my_object" {
+  bucket = "kakashka"
+  key    = "files/lapka.txt"
+  source = "files/lapka.txt" 
+}
+output "presigned_url" {
+  value = "https://kakashka.s3.eu-north-1.amazonaws.com/files/lapka.txt"
+}
